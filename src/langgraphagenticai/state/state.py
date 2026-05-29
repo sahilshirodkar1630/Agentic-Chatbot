074e23f18b640e714b1014850eba0a42427010c7
Sahil Shirodkar <sahilshirodkar1630@gmail.com>
2026-05-30 04:28:50 +0530

    added langraph setup + streamlit integration

---
+++ src/langgraphagenticai/state/state.py
@@ -0,0 +1,11 @@
+
+from typing import Annotated
+from typing_extensions import TypedDict,list
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    """
+    Represent the structure of the state used in graph
+    """
+    message:Annotated[list,add_messages]
+
